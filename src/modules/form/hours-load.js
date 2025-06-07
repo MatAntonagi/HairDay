98802b7cb42e5b8113165237591d381a9a0390a2
Matheus Toledo <mtoledoz650@gmail.com>
2025-06-07 15:36:23 -0300

    Renderiza os horários.

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -2,6 +2,7 @@ import dayjs from "dayjs"
 
 import { openingHours } from "../../utils/opening-hours"
 
+const hours = document.getElementById("hours")
 
 export function hoursLoad({date}){
     const opening = openingHours.map((hour) => {
@@ -13,9 +14,17 @@ export function hoursLoad({date}){
         
         return {
             hour,
-            avaliable: isHourPast,
+            available: isHourPast,
         }
     })
 
-    console.log(opening)
+    // Renderiza os horários.
+    opening.forEach(({hour, available}) => {
+        const li = document.createElement("li")
+        li.classList.add("hour")
+        li.classList.add(available ? "hour-available" : "hour-unavailable")
+
+        li.textContent = hour
+        hours.append(li)
+    })
 }
