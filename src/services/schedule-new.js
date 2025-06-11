44766c8b965eae0d7ee881659c910f76c22c3866
Matheus Toledo <mtoledoz650@gmail.com>
2025-06-11 11:02:51 -0300

    Criando o serviço de agendamento.

---
+++ src/services/schedule-new.js
@@ -0,0 +1,17 @@
+import { apiConfig } from "./api-config.js";
+
+export async function scheduleNew({ id, name, when}){
+    try {
+        await fetch(`${apiConfig.baseURL}/schedules,`, {
+            method: 'POST',
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({ id, name, when }),
+        })
+
+        alert("Agendamento realizado com sucesso!")
+    } catch (error) {
+        alert("Não foi possivel agendar tente novamente mais tarde.")
+    }
+}
